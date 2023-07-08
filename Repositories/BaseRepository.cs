2f640874e2efc5ce9cfdd2ebf5eaef2092040a3d
kawtarzz <kawtaryazzouzi@gmail.com>
2023-07-08 16:21:06 -0500

    Add project files.

---
+++ Repositories/BaseRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.SqlClient;
+
+using Microsoft.Extensions.Configuration;
+
+namespace Streamish.Repositories
+{
+    //abstract: indicates that our BaseRepository class cannot be directly instantiated, but can ONLY be used by inheritance.
+    public abstract class BaseRepository
+    {
+        //NOTE: We mark the Connection property as protected to make it available to child classes, but inaccessible to any other code.
+        private readonly string _connectionString;
+
+        public BaseRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        protected SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_connectionString);
+            }
+        }
+    }
+}
---
+++ Repositories/IVideoRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+using Streamish.Models;
+
+namespace Streamish.Repositories
+{
+    public interface IVideoRepository
+    {
+        void Add(Video video);
+        void Add(Video video);
+        void Delete(int id);
+        void Delete(int id);
+        List<Video> GetAll();
+        List<Video> GetAll();
+        Video GetById(int id);
+        Video GetById(int id);
+        void Update(Video video);
+        void Update(Video video);
+    }
+}
